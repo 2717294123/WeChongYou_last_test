2633241d7954a8baa53a3fe5ad169b993b25c3c3
Yifan Wang <72053918+2717294123@users.noreply.github.com>
2021-08-31 17:10:43 +0800

    Create BlogMapper.java

---
+++ BLOG/src/main/java/com/wm/mapper/BlogMapper.java
@@ -0,0 +1,26 @@
+package com.wm.mapper;
+
+import com.wm.po.Blog;
+import com.wm.vo.BlogList;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+@Mapper
+public interface BlogMapper {
+
+    List<BlogList> findAllBlogList();
+    List<Blog> findAllBlog();
+    List<Blog> findPublishedBlog();
+    Blog findById(Long id);
+    List<Blog> findByType(Long id);
+    List<Blog> searchByTitle(String title);
+    List<String> findYear();
+    List<Blog> findByYear(String year);
+    Integer findBlogCount();
+    Integer insertBlog(Blog blog);
+
+    Integer updateBlog(Blog blog);
+    Integer updateViews(Blog blog);
+
+    Integer deleteById(Long id);
+}
